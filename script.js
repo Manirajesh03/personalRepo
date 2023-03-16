abb6dd4ffed34e1a0ce19f5b2e4e8e73eb9a21ac
Rajesh Macha <manirajeshmacha@gmail.com>
2023-03-16 11:46:26 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Tic Tac Toe");
+let turn ="X";
+function changeTurn{
+    return turn==="X"?"0":"X"
+}
+
+function checkWin{
+    let boxes=document.getElementsByClassName("box");
+    
+}
+
